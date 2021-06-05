b2365982aba1a4c29697cecf03607c7641e73a4e
HarikumarG <harikumarmgp@gmail.com>
2021-06-06 00:33:36 +0530

    Production-ready

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const accountsRouter = require('./routes/accounts');
 const discussionsRouter = require('./routes/discussions');
 const mongoConnect = require('./dao/mongodb-connection');
+const favicon = require("serve-favicon");
 
 const app = express();
+
+app.use(favicon(__dirname + '/assets/server.png'));
 
 app.use(cors())
 
@@ -16,6 +19,11 @@ app.use('/accounts',accountsRouter);
 
 //all "discussions" and its related routes will be routed to "discussionsRouter"
 app.use('/discussions-list',discussionsRouter);
+
+app.get("/", function (request, response) {
+    let data = "<h1>Server Started and running successfully</h1>";
+    response.send(data);
+});
 
 mongoConnect.connection();
 app.listen(process.env.PORT || 5000);
